55ae15d86926aa8d0b14818f20cfffa11878e1ca
Truc Chau <tc9538@cs.ship.edu>
2021-05-12 00:24:54 -0400

    jquery

--- js/index.js
+++ js/index.js
@@ -1,47 +1,128 @@
 /*
     index.js
  */
+$(document).ready(function(){
 
+"user strict";
 // var msg= "Hello JavaScript";
 // console.log(msg);
 
 // var resultsDiv = document.getElementById("results");
 // resultsDiv.innerHTML = "<p>This is from JavaScript</p>";
 
+var resultList = $("#resultList");
+resultList.text("This is from jQuery");
 
-var results = [{
-    name: "jQuery",
-    language: "JavaScript",
-    score: 4.5,
-    showLog: function(){
+var toggleButton = $("#toggleButton");
+toggleButton.on("click", function(){
+    resultList.toggle(500);
 
-    },
-    owner: {
-        login: "testabc",
-        id: 123456
+    if(toggleButton.text() == "Hide")
+    {
+        toggleButton.text("Show");
     }
-}, {
-    name: "jQuery UI",
-    language: "JavaScript",
-    score: 3.5,
-    showLog: function(){
+    else toggleButton.text("Hide");
+});
 
-    },
-    owner: {
-        login: "testabc",
-        id: 123456
-    }
 
-}];
+var listItems = $("header nav li").css("font-weight", "bold");
+listItems.css("font-size", "bold");
+listItems.filter(":first").css("font-size", "18px");
 
-for(var x = 0; x< results.length; x++)
-{
-        var result = results[x];
-        if(result.score < 4){
-            break;
+$("#gitHubSearchForm").on("submit", function(){
+
+    var searchPhrase = $("#searchPhrase").val();
+    var useStars = $("#useStars").val();
+    var langChoice = $("#langChoice").val();
+
+    if(searchPhrase){
+
+        resultList.text("Performing search...");
+        var gitHubSearch = "https://api.github.com/search/repositories?q=" + encodeURIComponent(searchPhrase);
+
+        if(langChoice != "All"){
+            gitHubSearch += "+language" + langChoice;
         }
-        console.log(result.name);
+
+        if(useStars){
+            gitHubSearch += "&sort=stars";
+        }
+        // var gitHubSearch = "https://api.github.com/search/repositories?q=jquery+language:javascript&sort=stars";
+        //this jquery version doesn't have success and fail functions
+        $.get(gitHubSearch)
+        // .success(function(r) {
+        // //console.log(r.items.length);
+        //     displayResults(r.items);
+        // })
+        // .fail(function(err) {
+        //     console.log("Failed to query GitHub");
+        // })
+        .done(function(r){
+            displayResults(r.items)
+        });
+    }
+    return false;
+});
+
+
+// var results = [{
+//     name: "jQuery",
+//     language: "JavaScript",
+//     score: 4.5,
+//     showLog: function(){
+
+//     },
+//     owner: {
+//         login: "testabc",
+//         id: 123456
+//     }
+// }, {
+//     name: "jQuery UI",
+//     language: "JavaScript",
+//     score: 3.5,
+//     showLog: function(){
+
+//     },
+//     owner: {
+//         login: "testabc",
+//         id: 123456
+//     }
+
+// }];
+function displayResults(results)
+{
+    resultList.empty();
+$.each(results, function(i, item){
+
+    var newResult = $("<div class='result'>" + 
+    "<div class='title'>" + item.name + "</div>" + 
+    "<div>Language: " + item.language + "</div>" + 
+    "<div>Owner: " + item.owner.login + "</div>" +
+    "</div>");
+
+    newResult.hover(function(){
+        //makde it darker
+        $(this).css("background-color", "lightgray");
+    }, function(){
+        //reverse
+        $(this).css("background-color", "transparent");
+    });
+
+    resultList.append(newResult);
+
+ });
 }
+
+
+
+// for(var x = 0; x< results.length; x++)
+// {
+//         var result = results[x];
+//         if(result.score < 4){
+//             break;
+//         }
+//         console.log(result.name);
+// }
 
 // console.log(results.length);
 // console.log(results[0].name);
@@ -95,3 +176,5 @@ for(var x = 0; x< results.length; x++)
 // }
 
 // testMe();
+
+});
